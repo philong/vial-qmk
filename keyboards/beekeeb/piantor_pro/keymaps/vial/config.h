20be772ad83bad5e3cd77ac9dbb007ac8da31ff7
Phi-Long Do <p.do@axelor.com>
2023-12-07 16:58:34 +0100

    Add Vial support to Piantor Pro

---
+++ keyboards/beekeeb/piantor_pro/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* keyboard uid */
+#define VIAL_KEYBOARD_UID {0x49, 0x32, 0xC4, 0xEF, 0x12, 0x96, 0xC8, 0x4F}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 4 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 0 }
+
+/* default layer count */
+#define DYNAMIC_KEYMAP_LAYER_COUNT 16
