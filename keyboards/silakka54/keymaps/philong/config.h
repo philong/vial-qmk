1c35a0a87ce83f3d0d9456e387cb8c607a49c9d7
Phi-Long Do <1766243+philong@users.noreply.github.com>
2026-07-03 13:28:28 +0200

    Restore default unlock/bootmagic for Silakka54
    Silakka54 is always physically 54 keys, no need to restrict it.
    It's more natural to reach to the physically outter most keys.

--- keyboards/silakka54/keymaps/philong/config.h
+++ keyboards/silakka54/keymaps/philong/config.h
@@ -4,13 +4,7 @@
 
 #define VIAL_KEYBOARD_UID {0x97, 0xD0, 0xA5, 0x97, 0x57, 0x48, 0xD0, 0x4F}
 
-// Q and P keys, so the same positions work on 36-key layouts too.
-#define VIAL_UNLOCK_COMBO_ROWS { 1, 6 }
-#define VIAL_UNLOCK_COMBO_COLS { 1, 1 }
-
-#define BOOTMAGIC_LITE_ROW 1
-#define BOOTMAGIC_LITE_COLUMN 1
-#define BOOTMAGIC_LITE_ROW_RIGHT 6
-#define BOOTMAGIC_LITE_COLUMN_RIGHT 1
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 1 }
 
 #define EE_HANDS
